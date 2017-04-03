ac229b268282f9bff56551de3a1d86345065529a
Harlan Brown <hbrown@nuxeo.com>
2017-04-03 15:23:38 -0500

    update for 8.3

--- src/main/java/org/nuxeo/sample/ValueChangeListenerBean.java
+++ src/main/java/org/nuxeo/sample/ValueChangeListenerBean.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *
  */
 
 package org.nuxeo.sample;
 
 import java.io.Serializable;
 import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -20,9 +21,25 @@ public class ValueChangeListenerBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private static final Log log = LogFactory.getLog(ValueChangeListenerBean.class);
+    private String selectedValue;
 
-    public void valueChange(ValueChangeEvent e){
-    	String s = (String) e.getNewValue();
+    public String getSelectedValue() {
+        return selectedValue;
+    }
+    public void setSelectedValue(String selectedValue) {
+        log.error(selectedValue);
+        this.selectedValue = selectedValue;
+    }
+
+    public void valueChange(ValueChangeEvent e) {
+        String s = (String) e.getNewValue();
         log.error(s);
     }
+    public void ajaxListener(AjaxBehaviorEvent e) {
+	log.error("ajaxListener");
+    }
+
+    //@In(create = true, required = false)
+    //protected transient CoreSession documentManager;
+
 }
